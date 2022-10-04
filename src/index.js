f6be61e4c6ba32b478bce1cd288a7706636bd030
Crisna <crisnabto@gmail.com>
2022-10-04 14:53:14 -0300

    Iniciando o projeto

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
+// primeiro commit
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
